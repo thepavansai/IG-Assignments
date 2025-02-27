88e52b2603f24979f379f9129b163aae8aa8cda1
G.Pavan Sai Sriram <93752325+thepavansai@users.noreply.github.com>
2025-02-27 17:16:51 +0530

    Update AuthorServiceImpl.java

--- streams/src/streamswithcollections/AuthorServiceImpl.java
+++ streams/src/streamswithcollections/AuthorServiceImpl.java
@@ -1,8 +1,8 @@
 package streamswithcollections;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,10 +29,26 @@ public class AuthorServiceImpl implements AuthorService{
 	@Override
 	public Long getMobileByAdhar(List<Author> authorList, Long adharCard) {
 		
-		return null;
+		return authorList.stream().filter(x->x.getAdharCard().equals(adharCard)).map(Author::getMobile).findFirst().orElse(0L);
 	}
 	public static void main(String []pss) {
 		//List<>
+		List<Author> al=  Arrays.asList(
+                new Author(123456789L, "S", "A", "I", LocalDate.of(2001, 1, 17), "Male", "Guntur", 1234567890L),
+                new Author(987654321L, "P", "S", "S", LocalDate.of(2005, 5, 15), "Female", "Ongole", 9876543210L),
+                new Author(456789123L, "W", "E", "S", LocalDate.of(2002, 3, 20), "Female", "Ponnur", 4567891230L),
+                new Author(321654987L, "B", "M", "J", LocalDate.of(2006, 7, 30), "Male", "BZA", 3216549870L),
+                new Author(3216549L, "P", "S", "J", LocalDate.of(2005, 6, 20), "Male", "Guntur",89216549870L));
+		AuthorServiceImpl asl= new AuthorServiceImpl();
+		System.out.println("Unique surnames");
+		System.out.println(asl.getUniqueSurnames(al));
+		System.out.println("By cities");
+		System.out.println(asl.getAuthorsByCity(al, "Guntur"));
+		System.out.println("Female Avg age");
+		System.out.println(asl.femaleAverageAge(al));
+		System.out.println("By Adhaar");
+		System.out.println(asl.getMobileByAdhar(al, 123456789L));
+	
 	}
 
 }
